4371ab5893e88c0788aa08d373a3d9ffa5f56b87
Thà Lê <43161363+Hknight-official@users.noreply.github.com>
2020-08-21 00:00:49 +0700

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
 	$apikey = '15876493476519537157'; //API key, lấy từ website calong.pro thay vào trong cặp dấu ''
 	// database Mysql config
-	$local_db = "localhost";
-	$user_db = "root";
-	$pass_db = "";
+	$local_db = "local";
+	$user_db = "user";
+	$pass_db = "pass";
 	$name_db = "api_thesieutoc";
 	# đừng đụng vào 
   $conn = new mysqli($local_db, $user_db, $pass_db, $name_db);
